d6a9a945228b311809e69425b0d5d6a15f0016ba
Shashank T K <shashanktk510@gmail.com>
2026-07-24 22:22:44 +0530

    BinarySearch algorithm

---
+++ src/algorithms/BinarySearch.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    static boolean find(int[]arr ,int target){
+        int st=0;
+        int end=arr.length;
+
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid]==target){
+                return true;
+            } else if (arr[mid]<target) {
+                st=mid+1;
+            } else  {
+                end=mid-1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int [] arr ={1, 2, 3, 5, 6, 22, 26, 64, 66, 88};
+
+
+        System.out.println(find(arr,1));
+
+    }
+
+}
