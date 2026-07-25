cbf3e70c3ed7de597630a125b596493668d916b7
Shashank T K <shashanktk510@gmail.com>
2026-07-25 14:36:41 +0530

    Problems on binary search ceiling of given number

---
+++ src/binarySearchProblem/CeilingNumber.java
@@ -0,0 +1,31 @@
+package binarySearchProblem;
+
+public class CeilingNumber {
+
+    static int celingNumber(int [] arr,int target){
+        int st=0;
+        int end=arr.length-1;
+        
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(arr[mid]==target){
+                return arr[mid];
+            } else if (arr[mid]<target) {
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+
+        }
+        
+        
+        return arr[st];
+    }
+
+    public static void main(String[] args) {
+            int [] arr={1,2,6,8,9,11,22,66,555};
+        System.out.println(celingNumber(arr,23));;
+    }
+
+
+}
